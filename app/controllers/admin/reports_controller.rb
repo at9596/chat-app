8750549dca7c57076aee47b2c92b6d795db599c6
abhishek-fullstackror <abhishekgangdhari@gmail.com>
2024-05-04 07:02:10 +0530

    download excel file

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -9,6 +9,16 @@ class Admin::ReportsController < ApplicationController
         send_data pdf_data, filename: "reports.pdf", type: "application/pdf", disposition: "inline"
     end
 
+    def generate_excel
+      users = User.all
+      spreadsheet_service = SpreadsheetService.new
+      excel_data = spreadsheet_service.generate_excel(users)
+      respond_to do |format|
+        # format.xlsx { send_data excel_data, filename: "users.xlsx" }
+        format.xlsx { send_data excel_data, filename: "users.xlsx", type: :xlsx }
+      end
+    end
+
     def details_generate_pdf
         reports = Report.includes(:post, :comment).all
         pdf_data = ReportPdfGenerator.details_generate(reports)
